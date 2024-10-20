b9919ebf76e2719b594c3344aa7fcf7b3fee64ab
RPNephilim <rajpriyadarshi3924@gmail.com>
2024-10-20 14:47:42 +0530

    Initial project setup

---
+++ src/main/java/com/rp/imps/PrisonSystem.java
@@ -0,0 +1,11 @@
+package com.rp.imps;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrisonSystem {
+    public static void main(String args[]){
+        SpringApplication.run(PrisonSystem.class, args);
+    }
+}
---
+++ src/main/java/com/rp/imps/controller/PrisonerController.java
@@ -0,0 +1,9 @@
+package com.rp.imps.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/prisoner")
+public class PrisonerController {
+}
